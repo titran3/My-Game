711b8f65395eb753da3a5fc257604dfb538cdc4f
titran3 <121971162+titran3@users.noreply.github.com>
2023-02-13 14:42:48 -0800

    added laser, gun, more lights

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -5,13 +5,13 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour
 {
     private float speed = 5.0f;
-    private float runningSpeed= 6.0f;
+    private float runningSpeed = 6.0f;
     private float horizontalInput;
     private float forwardInput;
     private float runningCooldown = 3.5f;
-    private float timeStamp;
-    private float jumpSpeed= 2f;
-    private float xRange= 7.0f;
+    private float jumpSpeed = 2f;
+    private float xRange = 8.0f;
+    private float zRange = 6f;
     private Rigidbody playerRb;
     // Start is called before the first frame update
     void Start()
@@ -22,21 +22,22 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        timeStamp = Time.time + runningCooldown;
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
 
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         transform.Translate(Vector3.right * speed * horizontalInput * Time.deltaTime);
 
-        if (Input.GetKey(KeyCode.LeftShift)){
-         transform.Translate(Vector3.forward * Time.deltaTime * runningSpeed * forwardInput);
-         transform.Translate(Vector3.right * runningSpeed * horizontalInput * Time.deltaTime);
-         }
-         if (Input.GetKey(KeyCode.Space)){
-        playerRb.AddForce(Vector3.up * Time.deltaTime * jumpSpeed);
-         }
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * runningSpeed * forwardInput);
+            transform.Translate(Vector3.right * runningSpeed * horizontalInput * Time.deltaTime);
         }
+        if (Input.GetKey(KeyCode.Space))
+        {
+            playerRb.AddForce(Vector3.up * Time.deltaTime * jumpSpeed);
+        }
+
 
         if (transform.position.x < -xRange)
         {
@@ -45,5 +46,15 @@ public class PlayerControl : MonoBehaviour
         if (transform.position.x > xRange)
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
-        }   
-    }   
+        }
+
+        if (transform.position.z < -zRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zRange);
+        }
+        if (transform.position.z > zRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zRange);
+        }
+    }
+}
