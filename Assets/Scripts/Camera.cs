015b9cb6e0e7c48997d033ed400cfddd78016166
titran3 <121971162+titran3@users.noreply.github.com>
2023-02-15 11:36:50 -0800

    Better Camera

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -5,10 +5,18 @@ using UnityEngine;
 public class Camera : MonoBehaviour
 {
     public GameObject player;
+    private float smoothTime = 0.25f;
     private Vector3 offset = new Vector3(0, 10, 0);
+    private Vector3 velocity = Vector3.zero;
+    public float zRange = 1f;
+    public float xRange = 1f;
+
+    [SerializeField] private Transform target;
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.position = player.transform.position + offset;
+        Vector3 targetposition = target.position + offset;
+        transform.position = Vector3.SmoothDamp(transform.position, targetposition, ref velocity, smoothTime);
+    
     }
 }
